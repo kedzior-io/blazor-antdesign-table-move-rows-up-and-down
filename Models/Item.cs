7127d477198f7c03127845bc2bd2e83119f47883
Artur <artur@kedzior.es>
2021-04-21 18:27:45 +0200

    initial

---
+++ Models/Item.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AntDesignBlazorTableSortOrder.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int SortOrder { get; set; }
+
+        public static List<Item> CreateRandomList()
+        {
+            var list = new List<Item>
+            {
+                new Item
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Krew elfów",
+                    SortOrder = 1
+                },
+
+                new Item
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Czas pogardy",
+                    SortOrder = 2
+                },
+
+                new Item
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Chrzest ognia",
+                    SortOrder = 3
+                },
+
+                new Item
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wieża Jaskółki",
+                    SortOrder = 4
+                },
+
+                new Item
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Pani Jeziora",
+                    SortOrder = 5
+                }
+            };
+
+            return list;
+        }
+    }
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,56 @@
+﻿using AntDesignBlazorTableSortOrder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AntDesignBlazorTableSortOrder.Pages
+{
+    public partial class Index
+    {
+        public List<Item> Items { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Items = Item.CreateRandomList();
+        }
+
+        private void SortUp(Guid id)
+        {
+            var itemToUpdate = Items.Single(x => x.Id == id);
+            var index = Items.IndexOf(itemToUpdate);
+
+            if (index == 0)
+            {
+                return;
+            }
+
+            var newIndex = index - 1;
+
+            Items = SwapSortOrders(Items, index, newIndex);
+        }
+
+        private void SortDown(Guid id)
+        {
+            var itemToUpdate = Items.Single(x => x.Id == id);
+            var index = Items.IndexOf(itemToUpdate);
+
+            if (index == Items.Count - 1)
+            {
+                return;
+            }
+
+            var newIndex = index + 1;
+
+            Items = SwapSortOrders(Items, index, newIndex);
+        }
+
+        public static List<Item> SwapSortOrders(List<Item> list, int indexA, int indexB)
+        {
+            var tmp = list[indexA].SortOrder;
+            list[indexA].SortOrder = list[indexB].SortOrder;
+            list[indexB].SortOrder = tmp;
+
+            return list.OrderBy(x => x.SortOrder).ToList();
+        }
+    }
+}
